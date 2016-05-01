675bd69c2bb25eae2c54b4dd01e01df874436bd4
hannahdelawter <delawthc@dukes.jmu.edu>
2016-05-01 01:33:23 -0400

    Rename hamming.js to 1hamming.js

---
+++ 1hamming.js
@@ -0,0 +1,9 @@
+exports.compute = function(input1, input2){
+		var diff = 0;
+		for (i = 0; i < input1.length; i++) {
+			if (input1[i] != input2[i]) {
+				diff = diff + 1;
+			}
+		}
+			return diff;
+	};
